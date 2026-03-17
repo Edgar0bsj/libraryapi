d30a7ce00b0703f3cb882838e6f94fbfe418cf6a
Edgar0bsj <edgarbsj.dev@gmail.com>
2026-03-17 07:27:58 -0300

    feat(controller): criação do AutorController
    - Adicionado novo controller em 'libraryapi/controller/AutorController.java'
    - Definido endpoint base '/autores'
    - Implementado método POST '/autores' para salvar autores (ainda sem lógica de persistência)
    - Estrutura inicial preparada para futuras implementações de regras de negócio

---
+++ src/main/java/com/edgar/github/libraryapi/controller/AutorController.java
@@ -0,0 +1,14 @@
+package com.edgar.github.libraryapi.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/autores")
+public class AutorController {
+
+    @PostMapping
+    public void salvar(){}
+}
+
