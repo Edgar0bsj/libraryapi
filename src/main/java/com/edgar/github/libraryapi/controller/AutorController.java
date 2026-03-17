64a2e0f672639739dae213f389a02f39293c8eb8
Edgar0bsj <edgarbsj.dev@gmail.com>
2026-03-17 15:59:19 -0300

    feat(controller): retorno com Location header após criação de Autor
    - Atualizado AutorController:
      - Método salvar agora retorna ResponseEntity<Void> com status CREATED
      - Adicionado uso de ServletUriComponentsBuilder para construir URI do recurso recém-criado
      - Location header aponta para /autores/{id} do autor persistido
    
    Essa alteração segue boas práticas REST, permitindo que o cliente saiba
    onde acessar o recurso recém-criado, garantindo maior aderência ao padrão
    HTTP e melhorando a usabilidade da API.

--- src/main/java/com/edgar/github/libraryapi/controller/AutorController.java
+++ src/main/java/com/edgar/github/libraryapi/controller/AutorController.java
@@ -4,12 +4,15 @@ import com.edgar.github.libraryapi.dto.AutorDTO;
 import com.edgar.github.libraryapi.model.Autor;
 import com.edgar.github.libraryapi.service.AutorService;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.net.URL;
 
 @RestController
 @RequestMapping("/autores")
@@ -22,11 +25,18 @@ public class AutorController {
     }
 
     @PostMapping
-    public ResponseEntity salvar(@RequestBody AutorDTO autor){
+    public ResponseEntity<Void> salvar(@RequestBody AutorDTO autor){
 
         Autor autorEntity = autor.mapearParaAutor();
         service.salvar(autorEntity);
 
-        return new ResponseEntity("Teste: " + autor, HttpStatus.CREATED);
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(autorEntity.getId())
+                .toUri();
+
+
+        return ResponseEntity.created(location).build();
     }
 }
