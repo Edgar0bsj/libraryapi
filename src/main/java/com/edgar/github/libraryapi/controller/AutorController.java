efd57ff5a9f3ccdc47c2aab9dc06a6abd58c9fc0
Edgar0bsj <edgarbsj.dev@gmail.com>
2026-03-17 07:50:17 -0300

    feat(controller+dto): implementação de AutorController e AutorDTO
    - Modificado AutorController para receber AutorDTO via @RequestBody
    - Adicionado retorno ResponseEntity com status CREATED e mensagem de teste
    - Criado novo record AutorDTO contendo:
      - nome (String)
      - dataNascimento (LocalDate)
      - nacionalidade (String)
    - Estrutura inicial pronta para evoluir a lógica de persistência e validação
    - requisição do tipo post com o body:
    {
        "nome":"autor 1",
        "dataNascimento":"1999-03-31",
        "nacionalidade":"Brasileira"
    }
    recebendo com sucesso!

--- src/main/java/com/edgar/github/libraryapi/controller/AutorController.java
+++ src/main/java/com/edgar/github/libraryapi/controller/AutorController.java
@@ -1,6 +1,11 @@
 package com.edgar.github.libraryapi.controller;
 
+import com.edgar.github.libraryapi.dto.AutorDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class AutorController {
 
     @PostMapping
-    public void salvar(){}
-}
+    public ResponseEntity salvar(@RequestBody AutorDTO autor){
 
+        return new ResponseEntity("Teste: " + autor, HttpStatus.CREATED);
+    }
+}
