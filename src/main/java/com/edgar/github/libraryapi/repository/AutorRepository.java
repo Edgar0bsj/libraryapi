c7d90baa36512754b81b59dc97a16dabd0eef80f
Edgar0bsj <edgarbsj.dev@gmail.com>
2026-03-17 15:32:11 -0300

    feat(repository): criação de AutorRepository e LivroRepository
    - Criado AutorRepository estendendo JpaRepository<Autor, UUID>
      - Permite operações CRUD e consultas sobre a entidade Autor
    
    - Criado LivroRepository estendendo JpaRepository<Livro, UUID>
      - Permite operações CRUD e consultas sobre a entidade Livro
    
    Esses repositórios estabelecem a camada de acesso a dados,
    facilitando a persistência e recuperação de informações
    diretamente do banco de dados via Spring Data JPA.

---
+++ src/main/java/com/edgar/github/libraryapi/repository/AutorRepository.java
@@ -0,0 +1,9 @@
+package com.edgar.github.libraryapi.repository;
+
+import com.edgar.github.libraryapi.model.Autor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface AutorRepository extends JpaRepository<Autor, UUID> {
+}
---
+++ src/main/java/com/edgar/github/libraryapi/repository/LivroRepository.java
@@ -0,0 +1,9 @@
+package com.edgar.github.libraryapi.repository;
+
+import com.edgar.github.libraryapi.model.Livro;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface LivroRepository extends JpaRepository<Livro, UUID> {
+}
