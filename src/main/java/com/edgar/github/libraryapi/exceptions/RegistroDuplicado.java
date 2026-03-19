9238d9b4a8db866084c2418393f42e180d097f45
Edgar0bsj <edgarbsj.dev@gmail.com>
2026-03-19 18:55:25 -0300

    feat(exception): criação de exceção RegistroDuplicado
    - Criada classe RegistroDuplicado estendendo RuntimeException
      - Construtor recebe mensagem descritiva do erro
      - Exceção destinada a sinalizar tentativas de inserção de registros já existentes
    
    Essa alteração adiciona suporte a tratamento específico de
    conflitos de duplicidade, permitindo respostas mais claras
    e padronizadas para os clientes da API.- Criada classe RegistroDuplicado estendendo RuntimeException
      - Construtor recebe mensagem descritiva do erro
      - Exceção destinada a sinalizar tentativas de inserção de registros já existentes
    
    Essa alteração adiciona suporte a tratamento específico de
    conflitos de duplicidade, permitindo respostas mais claras
    e padronizadas para os clientes da API.

---
+++ src/main/java/com/edgar/github/libraryapi/exceptions/RegistroDuplicado.java
@@ -0,0 +1,7 @@
+package com.edgar.github.libraryapi.exceptions;
+
+public class RegistroDuplicado extends RuntimeException{
+    public RegistroDuplicado(String message) {
+        super(message);
+    }
+}
