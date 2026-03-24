e280c8504cc1aaaa368546d960d1939e14abc1c6
Edgar0bsj <edgarbsj.dev@gmail.com>
2026-03-24 09:26:22 -0300

    feat(validation): aprimoramento das constraints em AutorDTO
    - AutorDTO:
      - Campo nome:
        - @NotBlank(message = "Campo obrigatório")
        - @Size(min = 2, max = 100, message = "Campo fora do tamanho padrão")
      - Campo dataNascimento:
        - @NotNull(message = "Campo obrigatório")
        - @Past(message = "Não pode ser uma data futura")
      - Campo nacionalidade:
        - @NotBlank(message = "Campo obrigatório")
        - @Size(min = 2, max = 50, message = "Campo fora do tamanho padrão")
    
    Essas alterações garantem maior robustez na validação dos dados,
    evitando valores nulos, strings vazias, tamanhos fora do padrão
    e datas inválidas (futuras).

--- src/main/java/com/edgar/github/libraryapi/dto/AutorDTO.java
+++ src/main/java/com/edgar/github/libraryapi/dto/AutorDTO.java
@@ -3,15 +3,20 @@ package com.edgar.github.libraryapi.dto;
 import com.edgar.github.libraryapi.model.Autor;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 
 public record AutorDTO(
         @NotBlank(message = "Campo obrigatório")
+        @Size(min = 2, max = 100, message = "Campo fora do tamanho padrão")
         String nome,
         @NotNull(message = "Campo obrigatório")
+        @Past(message = "Não pode ser uma data futura")
         LocalDate dataNascimento,
         @NotBlank(message = "Campo obrigatório")
+        @Size(min = 2, max = 50, message = "Campo fora do tamanho padrão")
         String nacionalidade
 ) {
     public Autor mapearParaAutor(){
